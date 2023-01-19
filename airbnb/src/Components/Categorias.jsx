2020baa7fb7199f9a1501ed3898610d1aa23d1e8
marciolucasjunior <109992150+marciolucasjunior@users.noreply.github.com>
2023-01-19 17:27:30 -0300

    slide

--- airbnb/src/Components/Categorias.jsx
+++ airbnb/src/Components/Categorias.jsx
@@ -1,4 +1,17 @@
 import React from 'react'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+
+// import required modules
+import { Pagination } from "swiper";
+
 //importar css
 import './CSS/Categorias.css'
 
@@ -7,7 +20,25 @@ export default function Categorias() {
     <div className='container-fluid border border-danger'>
         <div className='container-airbnb border boder-info row'>
             <div className='col-sm-11'>
-                 Aqui vai categoria
+            <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
             </div>
             <div className='col-sm-1'>
                Aqui filtro
