4d506c9d2e6af8ac8ffc3b8fe05dd0dc32aa66fe
marciolucasjunior <109992150+marciolucasjunior@users.noreply.github.com>
2023-01-26 16:34:14 -0300

    yes

--- airbnb/src/Components/PriceSlider.jsx
+++ airbnb/src/Components/PriceSlider.jsx
@@ -4,19 +4,33 @@ import  './CSS/PriceSlider.css'
 export default function PriceSlider() {
   return (
     <div>
+
         <div className='slider'>
-
            <div className='progress'></div>
-
            <div className='range-input'>
-
             <input id='rangeMax'  type="range" />
-
             <input id='rangeMin' type="range" />
-
            </div>
-
         </div>
+
+        <div className='row mt-4'>
+          <div className='col'>
+            <label className='text-muted' >preço minimo</label>
+            <div class="input-group mb-3">
+           <span class="input-group-text" id="basic-addon1">R$</span>
+           <input id='inputMin' type="text" class="form-control" placeholder="Min" />
+           </div>
+          </div>
+
+          <div className='col'>
+            <label className='text-muted' >preço máximo</label>
+            <div class="input-group mb-3">
+           <span class="input-group-text" id="basic-addon1">R$</span>
+           <input id='inputMin' type="text" class="form-control" placeholder="Max" />
+           </div>
+          </div>
+        </div>
+
     </div>
   )
 }
