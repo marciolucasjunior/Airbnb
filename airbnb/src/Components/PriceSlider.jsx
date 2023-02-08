713aedda76b2674265bb494a6168dc42375605be
marciolucasjunior <109992150+marciolucasjunior@users.noreply.github.com>
2023-02-08 17:02:31 -0300

    validações

--- airbnb/src/Components/PriceSlider.jsx
+++ airbnb/src/Components/PriceSlider.jsx
@@ -10,6 +10,10 @@ export default function PriceSlider(props) {
  const [right,setRight] = useState('0%')
 
  function changeRangeMin(e){
+    
+  if((parseInt(e.target.value) - max) >= -props.step){
+     console.log('Limite minimo alcançado')
+  }else {
     setMin(parseInt(e.target.value))
     if(min == props.min){
       setLeft('0%')
@@ -20,18 +24,30 @@ export default function PriceSlider(props) {
       setLeft(((qtsSteps * 100) / totalBarra) + '%')
     }
 
+  }
+
+  
  }
 
  function changeRangeMax(e){
-  setMax(parseInt(e.target.value))
-  if(min == props.max){
-    setRight('0%')
-  }else{
-    var totalBarra = props.max - props.min;
-    var qtsSteps = props.max - max;
 
-    setRight(((qtsSteps * 100) / totalBarra) + '%')
+  if((parseInt(e.target.value) - min) <= props.step){
+    console.log('limite Maximo alcançado')
+  } else {
+    setMax(parseInt(e.target.value))
+    if(min == props.max){
+      setRight('0%')
+    }else{
+      var totalBarra = props.max - props.min;
+      var qtsSteps = props.max - max;
+  
+      setRight(((qtsSteps * 100) / totalBarra) + '%')
+    }
   }
+  
+   
+ 
+  
 
 }
 
@@ -52,6 +68,55 @@ export default function PriceSlider(props) {
     }
    },[max]);
 
+   function validacao(e){
+    
+    //CAMPO UNPUT MIN
+    let inputMin = document.getElementById('inputMin')
+    //PEGAR O VALOR DO CAMPO INPUT
+    let valor = parseInt(e.target.value)
+    //SE O CAMPO INPUT FOR INPUT MIN
+    if(e.target.id == 'inputMin'){
+      //VERIFICAR SE O CAMPO É VAZIO OU NULL
+      if(e.target.value=="" || e.target.value==null){
+        //SETAR PARA O MÍNIMO PERMITIDO
+        setMin(props.min);
+        setLeft('0%')
+        //MUDAR O VALOR DO CAMPO INPUTMIN PARA VALOR MÍNIMO
+        inputMin.value = props.min
+      }
+      //VERIFICAR SE VALOR É MENOR QUE MÍNIMO
+      if(valor < props.min){setMin(props.min);
+        setLeft('0%')
+        //MUDAR O VALOR DO CAMPO INPUTMIN PARA VALOR MÍNIMO
+        inputMin.value = props.min
+
+      }else {
+        //VALOR É MAIOR QUE MINIMO PERMITIDO - OK
+        //VERIFICAR SE MIN É MAIOR QUE MAX
+        if(valor >=max){
+          let corrige = max - props.step;
+          setMin(corrige);
+
+          var totalBarra = props.max - props.min;
+          var qtsSteps = valor - props.min
+
+
+          setLeft(((qtsSteps * 100) / totalBarra) +'%');
+
+          inputMin.value = corrige;
+        }else{
+          //TUDO CERTO
+          setMin(valor);
+          //MOVIMENTAR A BARRA
+          var totalBarra = props.max - props.min;
+          var qtsSteps = valor - props.min
+
+
+          setLeft(((qtsSteps * 100) / totalBarra) +'%');
+        }
+      }
+    }
+   }
 
   return (
     <div>
@@ -71,7 +136,7 @@ export default function PriceSlider(props) {
             <label className='text-muted' >preço minimo</label>
             <div class="input-group mb-3">
            <span class="input-group-text" id="basic-addon1">R$</span>
-           <input id='inputMin' type="text" class="form-control" placeholder="Min" />
+           <input id='inputMin' onBlur={validacao} type="text" class="form-control" placeholder="Min" />
            </div>
           </div>
 
